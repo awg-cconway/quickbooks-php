b46a15e49a3d4dad5e4e9f0e7ed4c0f1df9a2f37
conway_chris <cconway@activewebgroup.com>
2025-04-30 15:42:20 -0400

    fixes from rpicorp/quickbooks-php

--- QuickBooks/IPP/Service/Attachable.php
+++ QuickBooks/IPP/Service/Attachable.php
@@ -21,9 +21,59 @@ QuickBooks_Loader::load('/QuickBooks/IPP/Service.php');
 
 class QuickBooks_IPP_Service_Attachable extends QuickBooks_IPP_Service
 {
+	/**
+	 * Resource.
+	 *
+	 * @var string
+	 */
+	private $_resource = 'Attachable';
+
+	/**
+	 * Add.
+	 *
+	 * @param QuickBooks_IPP_Context $Context Context.
+	 * @param string                 $realmID Company ID.
+	 * @param object                 $Object  Object.
+	 *
+	 * @return integer
+	 */
+	public function add($Context, $realmID, $Object)
+	{
+		return $this->_add($Context, $realmID, $this->_resource, $Object);
+	}
+
+	/**
+	 * Updates attachable.
+	 *
+	 * @param object $Context Context.
+	 * @param string $realmID Company Id.
+	 * @param string $IDType  Resource.
+	 * @param object $Object  Object.
+	 *
+	 * @return boolean
+	 */
+	public function update($Context, $realmID, $IDType, $Object)
+	{
+		return $this->_update($Context, $realmID, $this->_resource, $Object, $IDType);
+	}
+
 	public function query($Context, $realm, $query)
 	{
-		return parent::_query($Context, $realm, $query);
+		return $this->_query($Context, $realm, $query);
+	}
+
+	/**
+	 * Deletes attachable.
+	 *
+	 * @param object $Context Context.
+	 * @param string $realmID Company Id.
+	 * @param string $IDType  Resource.
+	 *
+	 * @return boolean
+	 */
+	public function delete($Context, $realmID, $IDType)
+	{
+		return $this->_delete($Context, $realmID, 'Attachable', $IDType);
 	}
 
 	public function download($Context, $realmID, $ID)
